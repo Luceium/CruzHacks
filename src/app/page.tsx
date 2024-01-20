d0b0dea5a83c06bc74895abe445c43a2f11693b6
Steven Le <stevenleusa79@yahoo.com>
2024-01-20 07:02:59 -0800

    added middleware function for auth, server side auth for landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
+import { NextPage } from "next/types";
 import prisma from "../lib/prisma"; 
+import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 
 async function getEvents() {
   const events = await prisma.event.findMany();
@@ -24,38 +26,42 @@ const EventComponent = (event : any) => {
   )
 }
 
-export default async function Home() {
-  const events = await getEvents();
-  return (
-    <main className="flex flex-col items-center min-h-[calc(100vh)]">
+const Home: NextPage = withPageAuthRequired(
+  async () => {
+    const events = await getEvents();
 
-      <h2 className="p-8 md:p-12 text-4xl text-center font-extrabold tracking-tight text-gray-700 font-outline-2 lg:text-4xl">
-        Slug it Out at Your Next Event...
-      </h2>
-      <div className="bg-primary rounded-lg shadow-md border-white border-4
-      w-[90%] md:w-[80%] grid lg:grid-cols-3 gap-2 overflow-y-auto  min-h-[calc(50vh)] max-h-[55vh] p-4">
-        {events.length > 0 ? (
-          events.map((event) => (
-            <EventComponent key={event.id} event={event} />
-          ))
-        ) : (
-          <div className="flex flex-col col-span-3">
-            <div className="text-3xl md:text-2xl text-center justify-center text-primary-content col-span-3 md:p-12">
-              No events are currently available. 
-              <br/><br/> 
-              Click the button below to create a new event for others to join!
+    return (
+      <main className="flex flex-col items-center min-h-[calc(100vh)]">
+        <h2 className="p-8 md:p-12 text-4xl text-center font-extrabold tracking-tight text-gray-700 font-outline-2 lg:text-4xl">
+          Slug it Out at Your Next Event...
+        </h2>
+        <div className="bg-primary rounded-lg shadow-md border-white border-4
+          w-[90%] md:w-[80%] grid lg:grid-cols-3 gap-2 overflow-y-auto  min-h-[calc(50vh)] max-h-[55vh] p-4">
+          {events.length > 0 ? (
+            events.map((event) => (
+              <EventComponent key={event.id} event={event} />
+            ))
+          ) : (
+            <div className="flex flex-col col-span-3">
+              <div className="text-3xl md:text-2xl text-center justify-center text-primary-content col-span-3 md:p-12">
+                No events are currently available. 
+                <br/><br/> 
+                Click the button below to create a new event for others to join!
+              </div>
+              <div className="flex h-full justify-center items-center">
+                <a href="/addEvent" className="p-4">
+                  <button className="btn bg-secondary text-xl border-0">
+                    Add Events
+                  </button>
+                </a>
+              </div>
             </div>
-            <div className="flex h-full justify-center items-center">
-              <a href="/addEvent" className="p-4">
-                <button className="btn bg-secondary text-xl border-0">
-                  Add Events
-                </button>
-              </a>
-            </div>
-          </div>
-        )}
-      </div>
-      
-    </main>
-  );
-}
+          )}
+        </div>
+      </main>
+    );
+  },
+  { returnTo: "/" },
+);
+
+export default Home;
---
+++ src/middleware.ts
@@ -0,0 +1,9 @@
+import { withMiddlewareAuthRequired } from "@auth0/nextjs-auth0/edge";
+
+export default withMiddlewareAuthRequired();
+
+
+// middleware --> routes under /protected or /admin are now protected...
+export const config = {
+  matcher: ["/admin"],
+};
