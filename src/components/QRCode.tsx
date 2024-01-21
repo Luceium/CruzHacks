fbb84f3ae6d292279644f48beb79c2f4236526a2
Steven Le <stevenleusa79@yahoo.com>
2024-01-20 22:26:53 -0800

    optimize qr code

--- src/components/QRCode.tsx
+++ src/components/QRCode.tsx
@@ -30,7 +30,7 @@ export default function QRCodeComponent({size} : {size: number}) {
   return(
     <button className="bg-white rounded-xl text-primary-content text-sm p-1 border:none max-w-32" 
     onClick={()=>toggle(true)}>
-      <Image src={`https://api.qrserver.com/v1/create-qr-code/?data=${pathname}&amp;size=${size}x${size}`} alt="" width={size} height={size}/>
+      <Image src={`https://api.qrserver.com/v1/create-qr-code/?data=${pathname}&amp;size=${size}x${size}`} alt="" width={size} height={size} priority unoptimized={true}/>
       {<dialog ref={ref} className={classNames("modal", {"modal-open": isToggled})}>
           <div className="modal-box h-fit bg-white text-center">
             <Image src={`https://api.qrserver.com/v1/create-qr-code/?data=${pathname}&amp;size=${size}x${size}`} alt="" width="500" height="500"/>
