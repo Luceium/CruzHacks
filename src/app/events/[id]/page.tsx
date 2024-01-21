6c498a76c132acb0812b230886c24598f84d51fb
Mauricio Curiel <mauriciom.curiel@gmail.com>
2024-01-20 18:43:41 -0800

    conditinally renders event page role based views

--- src/app/events/[id]/page.tsx
+++ src/app/events/[id]/page.tsx
@@ -1,6 +1,7 @@
 import prisma from "@/lib/prisma"
 import AdminEvent from "./adminEvent"
 import UserEvent from "./userEvent"
+import { getSession } from "@auth0/nextjs-auth0"
 
 export default async function Page({ params }: { params: { id: string} }) {
     const id = params.id
@@ -18,9 +19,10 @@ export default async function Page({ params }: { params: { id: string} }) {
         return false    
     }
 
+   const email = (await getSession())!.user.email
    const admins = eventData.admins
    const users = eventData.users
-   const duh = true
+   const isAdmin = admins.some(a => a.email == email)
 
-   return (duh ? <AdminEvent admins={admins} users={users} event={eventData}/> : <UserEvent/>);
+   return (isAdmin ? <AdminEvent admins={admins} users={users} event={eventData}/> : <UserEvent/>);
   }
