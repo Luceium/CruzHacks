34c03c6b89f513f88c63bfe5a3372bcaa4f7d829
Mauricio Curiel <mauriciom.curiel@gmail.com>
2024-01-22 20:52:54 -0800

    refactors enforce to be used with home page as well

--- src/util/enforce.tsx
+++ src/util/enforce.tsx
@@ -2,9 +2,17 @@ import React from "react";
 import { getSession } from "@auth0/nextjs-auth0";
 import { getUserByEmail } from "./getUserByEmail";
 import UpdateAccountForm from "@/components/UpdateAccountForm";
+import { redirect } from "next/navigation";
 
-export default async function Enforce({children} : any) {
-        const {user} = (await getSession())!;
-        const userInfo = await getUserByEmail(user.email);
-        return (<>{userInfo ? children : <UpdateAccountForm softRedirect/>}</>);
+export default async function Enforce({children, shouldRedirect} : {children: any, shouldRedirect?: boolean}) {
+        const session = await getSession();
+        
+        // redirect from home to events if logged in
+        if (shouldRedirect) {
+                if (session) redirect('/events')
+                return children
+        }
+        
+        const completedAccount = session ? await getUserByEmail(session.user.email) : null
+        return (<>{completedAccount ? children : <UpdateAccountForm softRedirect/>}</>);
 }
