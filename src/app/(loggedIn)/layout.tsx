dbec4351b6407f725ed415eaed7dff9c3f08439e
Mauricio Curiel <mauriciom.curiel@gmail.com>
2024-01-23 00:20:22 -0800

    rewrites middleware to not block manifest and pwa related links

--- src/app/(loggedIn)/layout.tsx
+++ src/app/(loggedIn)/layout.tsx
@@ -1,4 +1,3 @@
-import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "../globals.css";
 import { UserProvider } from '@auth0/nextjs-auth0/client';
@@ -6,51 +5,6 @@ import Navbar from "@/components/Navbar";
 import Enforce from "@/util/enforce";
 
 const inter = Inter({ subsets: ["latin"] });
-
-const APP_NAME = "Safe And Slug";
-const APP_DEFAULT_TITLE = "Safe And Slug";
-const APP_TITLE_TEMPLATE = "%s - PWA App";
-const APP_DESCRIPTION = "Attend UCSC events safe and sound!";
-
-export const metadata: Metadata = {
-  applicationName: APP_NAME,
-  title: {
-    default: APP_DEFAULT_TITLE,
-    template: APP_TITLE_TEMPLATE,
-  },
-  description: APP_DESCRIPTION,
-  manifest: "/manifest.json",
-  appleWebApp: {
-    capable: true,
-    statusBarStyle: "default",
-    title: APP_DEFAULT_TITLE,
-    // startUpImage: [],
-  },
-  formatDetection: {
-    telephone: false,
-  },
-  openGraph: {
-    type: "website",
-    siteName: APP_NAME,
-    title: {
-      default: APP_DEFAULT_TITLE,
-      template: APP_TITLE_TEMPLATE,
-    },
-    description: APP_DESCRIPTION,
-  },
-  twitter: {
-    card: "summary",
-    title: {
-      default: APP_DEFAULT_TITLE,
-      template: APP_TITLE_TEMPLATE,
-    },
-    description: APP_DESCRIPTION,
-  },
-};
-
-export const viewport: Viewport = {
-  themeColor: "#FFFFFF",
-};
 
 export default function RootLayout({
   children,
--- src/middleware.ts
+++ src/middleware.ts
@@ -4,5 +4,5 @@ export default withMiddlewareAuthRequired();
 
 // middleware --> routes under /protected or /admin are now protected...
 export const config = {
-    matcher: "/:path+",
+    matcher: "/((?!manifest.json|sw.js|logo72x72|logo192x192|logo384x384|logo512x512|api|favicon.ico).+)",
 };
