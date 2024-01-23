73da0849031d80b574f3fb1d5f6286cad04aa481
Mauricio Curiel <mauriciom.curiel@gmail.com>
2024-01-23 00:50:38 -0800

    adds button to download app

--- src/app/(loggedOut)/page.tsx
+++ src/app/(loggedOut)/page.tsx
@@ -1,10 +1,24 @@
+"use client"
 import React from 'react'
 
 const page = () => {
+  type BeforeInstallPromptEvent = any;
+
+  let installPrompt: BeforeInstallPromptEvent;
+  if (typeof window !== "undefined") {
+    window.addEventListener("beforeinstallprompt", (event) => {
+      event.preventDefault()
+      installPrompt = event
+    })
+  }
+
   return (
     <div className='h-screen flex flex-col items-center justify-center gap-10'>
         <button className="btn btn-wide"><a href="/api/auth/login">Log In</a></button>
-        <button className="btn btn-wide">Download App</button>
+        {/* TODO: only render in non-standalone and if browser supports download */}
+        <button className="btn btn-wide" onClick={() => {
+          installPrompt.prompt()
+        }} >Download App</button>
     </div>
   )
 }
