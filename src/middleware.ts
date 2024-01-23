8b06123d44e2c005e0a704c0f790ae731ca412a7
Mauricio Curiel <mauriciom.curiel@gmail.com>
2024-01-22 19:31:30 -0800

    moves events page

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,8 @@
 import { withMiddlewareAuthRequired } from "@auth0/nextjs-auth0/edge";
 
 export default withMiddlewareAuthRequired();
+
+// middleware --> routes under /protected or /admin are now protected...
+export const config = {
+    matcher: ["/.+"],
+};
